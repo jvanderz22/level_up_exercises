0e65c6604732b523237b9ff53aefe2a79c6de127
Jordan VanderZwaag <Jordan@clm-jvanderzwaag.enova.com>
2014-07-31 17:08:10 -0500

    Finished supportive

--- supportive/supportive.rb
+++ supportive/supportive.rb
@@ -1,6 +1,7 @@
 # In case you missed them, here are the extensions: http://guides.rubyonrails.org/active_support_core_extensions.html
 
 require 'active_support/all'
+require 'pry'
 
 class BlagPost
   attr_accessor :author, :comments, :categories, :body, :publish_date
@@ -9,15 +10,10 @@ class BlagPost
   DISALLOWED_CATEGORIES = [:selfposts, :gossip, :bildungsromane]
 
   def initialize(args)
-    args = args.inject({}) do |hash, (key, value)|
-      hash[key.to_sym] = value
-      hash
-    end
-
+    args.symbolize_keys!
     if args[:author] != '' && args[:author_url] != ''
       @author = Author.new(args[:author], args[:author_url])
     end
-
     if args[:categories]
       @categories = args[:categories].reject do |category|
         DISALLOWED_CATEGORIES.include? category
@@ -27,7 +23,7 @@ class BlagPost
     end
 
     @comments = args[:comments] || []
-    @body = args[:body].gsub(/\s{2,}|\n/, ' ').gsub(/^\s+/, '')
+    @body = args[:body].squish
     @publish_date = (args[:publish_date] && Date.parse(args[:publish_date])) || Date.today
   end
 
@@ -38,63 +34,30 @@ class BlagPost
   private
 
   def byline
-    if author.nil?
-      ""
-    else
-      "By #{author.name}, at #{author.url}"
-    end
+    author.try { "By #{author.name}, at #{author.url}" } || ""
   end
 
   def category_list
-    return "" if categories.empty?
-
-    if categories.length == 1
-      label = "Category"
-    else
-      label = "Categories"
-    end
-
-    if categories.length > 1
-      last_category = categories.pop
-      suffix = " and #{as_title(last_category)}"
-    else
-      suffix = ""
-    end
-
-    label + ": " + categories.map { |cat| as_title(cat) }.join(", ") + suffix
+    label = categories.empty? ? "" : "Category".pluralize(categories.length)
+    label + ": " + categories.map { |cat| as_title(cat) }.to_sentence
   end
 
   def as_title(string)
-    string = String(string)
-    words = string.gsub('_', ' ').split(' ')
-
-    words.map!(&:capitalize)
-    words.join(' ')
+    String(string).titleize
   end
 
   def commenters
     return '' unless comments_allowed?
     return '' unless comments.length > 0
-
-    ordinal = case comments.length % 10
-      when 1 then "st"
-      when 2 then "nd"
-      when 3 then "rd"
-      else "th"
-    end
-    "You will be the #{comments.length}#{ordinal} commenter"
+    "You will be the #{comments.length.ordinalize} commenter"
   end
 
   def comments_allowed?
-    publish_date + (365 * 3) > Date.today
+    publish_date.years_since(3) > Date.today
   end
 
   def abstract
-    if body.length < 200
-      body
-    else
-      body[0..200] + "..."
-    end
+    body.truncate(200)
   end
 
 end
